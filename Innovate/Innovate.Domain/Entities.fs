4a12fcdc0c88e1e02fb91f26b36a0624075a11b2
Cavill, Stuart <Stuart.Cavill@experian.com>
2016-03-08 22:09:15 +0000

    Initial commit of first entities

---
+++ Innovate/Innovate.Domain/Entities.fs
@@ -0,0 +1,15 @@
+﻿namespace Innovate.Domain
+
+type Category = {
+    CategoryId : int
+    CategoryText : string
+    CategoryOwner : string
+}
+
+type Suggestion = {
+    SuggestionId : int
+    SuggestionText : string
+    Submitter : string
+    Category : Category
+    Status : string
+}
